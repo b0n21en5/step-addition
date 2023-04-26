cf5d3732ec2fd51ea98e196b62b0ec8a88374605
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-04-26 17:07:53 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,14 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import numberRoute from "./routes/numberRoute.js";
-import mongoose from "mongoose";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// es-module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // rest objct
 const app = express();
@@ -16,24 +8,8 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 app.use("/api", numberRoute);
 
-// rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-dotenv.config();
-
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-  } catch (error) {}
-};
-
-app.listen(8800, () => {
-  connect();
-});
+app.listen(8800, () => {});
