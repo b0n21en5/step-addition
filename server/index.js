4f16c79257c6aa9fe570510ea1e2f0ee7806db4f
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-04-26 15:44:28 +0530

    deployment code

--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
+import dotenv from "dotenv";
 import numberRoute from "./routes/numberRoute.js";
-
-// es-module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import mongoose from "mongoose";
 
 // rest objct
 const app = express();
@@ -15,11 +10,18 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 app.use("/api", numberRoute);
 
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+  } catch (error) {}
+};
+
 app.listen(8800, () => {
-  console.log("Connected to backend!");
+  connect();
 });
