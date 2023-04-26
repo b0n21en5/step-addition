410502e0db3f258c3d26c104fc615f169f519e0d
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-04-26 15:03:28 +0530

    production code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import numberRoute from "./routes/numberRoute.js";
+
+// es-module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest objct
 const app = express();
@@ -9,6 +15,7 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 app.use("/api", numberRoute);
