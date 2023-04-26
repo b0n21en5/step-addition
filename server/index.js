31ddfa42ede007140a66dc7dedb23ca6d8f08452
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-04-26 16:08:28 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import numberRoute from "./routes/numberRoute.js";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// es-module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest objct
 const app = express();
@@ -10,9 +16,15 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 app.use("/api", numberRoute);
+
+// rest api
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 dotenv.config();
 
